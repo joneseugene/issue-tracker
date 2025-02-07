697caf6586b90d8d36952638a3dcf4f0394e40bd
joneseugene <joneseugeneceo@gmail.com>
2025-02-07 13:34:29 +0000

    Developed Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`} >
         <Navbar />
-        {children}
+        <main>
+          {children}
+        </main>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div >
-      <h1 className="text-red-600">Hello World...</h1>
+      <h1>Hello World...</h1>
     </div>
   );
 }
