365e3c036441399427509d805f7968bea3ef9e12
joneseugene <joneseugeneceo@gmail.com>
2025-02-07 14:24:59 +0000

    Modified the Navbar

--- app/layout/Navbar.jsx
+++ app/layout/Navbar.jsx
@@ -1,13 +1,21 @@
-import { link } from 'fs';
+'use client'
+
 import Link from 'next/link'
 import React from 'react'
 import { FaBug } from "react-icons/fa";
+import { usePathname } from 'next/navigation';
+import { classnames } from 'classnames'
 
 const Navbar = () => {
+    {/* Make Navbar Active */}
+    const activePath = usePathname()
+
+    {/* Make Navbar Dynamic*/}
     const links = [
         { label: 'Dashboard', href: '/'},
         { label: 'Issues', href: '/issues'}
     ]
+
   return (
     <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
         <Link href={"/"}><FaBug /></Link>
@@ -15,8 +23,14 @@ const Navbar = () => {
             {links.map(link => 
                 <Link 
                     key={link.href} 
-                    href={link.href}
-                    className='text-zinc-500 hover:text-zinc-800 transition-colors'>
+                    className = {
+                        classnames({
+                            'text-zinc-900': link.href === activePath,
+                            'text-zinc-500': link.href !== activePath,
+                            'hover:text-zinc-800 transition-colors': true
+                        })
+                    }
+                    href={link.href}>
                         {link.label}
                     </Link>
                 )}
