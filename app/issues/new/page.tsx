e009601e812644b7037580d88b6c749dc9d7d316
joneseugene <joneseugeneceo@gmail.com>
2025-02-08 00:52:29 +0000

    Server side error handling

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import { TextField, TextArea, Button } from '@radix-ui/themes'
-import React from 'react'
+import { TextField, Callout, Button } from '@radix-ui/themes'
+import React, { useState } from 'react'
 import "easymde/dist/easymde.min.css";
 import { useForm, Controller } from 'react-hook-form';
 import axios from 'axios';
@@ -20,17 +20,32 @@ interface IssueForm {
 const NewIssue = () => {
   const router = useRouter();
   const {register, control, handleSubmit} = useForm<IssueForm>();
+  const [ error, setError ] = useState('');
+
   return (
-    <form className='max-w- space-y-3' onSubmit={handleSubmit(async(data) => {
-      await axios.post('/api/issues', data);
-      router.push('/issues')
-    })}>
-        <TextField.Root placeholder="Title..." {...register('title')}/>
-        <Controller
-            name="description" control={control} render={({ field }) => <SimpleMDE placeholder="Description…" {...field} /> }
-        />
-        <Button>Submit</Button>
-    </form>
+    <div className="max-w-xl">
+        {error && (
+          <Callout.Root color="red" className="mb-5">
+            <Callout.Text>{error}</Callout.Text>
+          </Callout.Root>
+        )}
+        <form className='space-y-3' onSubmit={handleSubmit(async(data) => {
+          try {
+          await axios.post('/api/issues', data);
+          router.push('/issues')
+          } catch (error) {
+            console.log(error)
+            setError('An unexpected error occurred');
+          }
+          
+          })}>
+            <TextField.Root placeholder="Title..." {...register('title')}/>
+            <Controller
+                name="description" control={control} render={({ field }) => <SimpleMDE placeholder="Description…" {...field} /> }
+            />
+            <Button>Submit</Button>
+        </form>
+    </div>
   )
 }
 
